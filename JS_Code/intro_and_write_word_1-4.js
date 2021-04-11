ce3b3da8a933a99a127a59a2109305b2ca8b577d
Abdul Majid <fa19-bse-014@cuilahore.edu.pk>
2021-04-11 21:58:13 +0500

    Starting new journy with JS

---
+++ JS_Code/intro_and_write_word_1-4.js
@@ -0,0 +1,31 @@
+// Introduction
+// clint side  scripting language 
+// event based programming language 
+/*Click, Double Click , Right Click
+  Mouse hover, Mouse Out, Drag Drop
+  Key Press, Key Up, Load, Unload,
+  resize, Scroll */
+
+// Use in Web Dev
+/* Dropdown Menu
+   Maps 
+   Animations
+   chart - graph
+   pop up window
+   audio players
+   video players
+   zoom effect
+   animated gallery
+   form validation**
+   Accordions
+   Calendar
+*/
+
+//  COMMANDS
+// write commad
+document.write("Hello World ");
+document.write("Hello Majid!");
+
+//HTML tag in js file 
+document.write("<br>Hello World <br>");
+document.write("Hello <b><i>Majid!</i></b>");
---
+++ JS_Code/variables_5.js
@@ -0,0 +1,8 @@
+// Types of variables in JS
+// var , let , const
+
+var name = "<b>Abdul Majid</b>";
+document.write(name);
+
+var age = 20;
+document.write("<br>My age is " + age) //+ for concatination
